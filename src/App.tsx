ce9b733225c91b048945d1bad478d9c5c6f9ae02
Aurelien Cepede <aurelien@cepede.fr>
2020-09-09 09:33:41 +0200

    position absolute video

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,76 @@
-import React, { useEffect, useRef } from "react";
+import React, { RefObject, useEffect, useRef } from "react";
 import "./App.css";
 import WebcamStreamer from "./WebcamStreamer";
+import styled from "styled-components";
 
+const VideoStyle = styled.video`
+  position: absolute;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: auto;
+  opacity: 0.5;
+`;
+const WebcamStyle = styled.video`
+  width: 100%;
+  height: auto;
+`;
+
+const Container = styled.div`
+  position: relative;
+  width: 300px;
+  margin: auto;
+`;
+const startWebcam = (
+  webcamStreamer: WebcamStreamer,
+  webcamVideo: RefObject<HTMLVideoElement>
+) => {
+  if (
+    webcamVideo.current !== null &&
+    webcamVideo.current.srcObject !== null &&
+    "active" in webcamVideo.current.srcObject &&
+    webcamVideo.current.srcObject.active
+  ) {
+    return;
+  }
+  webcamStreamer.start().then((stream) => {
+    if (webcamVideo.current !== null) {
+      webcamVideo.current.srcObject = stream;
+    }
+  });
+};
 function App() {
   const webcamVideo = useRef<HTMLVideoElement>(null);
   const simpleVideo = useRef<HTMLVideoElement>(null);
   const webcamStreamer = new WebcamStreamer();
   useEffect(() => {
-    webcamStreamer.start().then((stream) => {
-      if (webcamVideo.current !== null) {
-        webcamVideo.current.srcObject = stream;
-      }
-    });
+    startWebcam(webcamStreamer, webcamVideo);
     return () => {
       webcamStreamer.stop();
     };
   }, [webcamStreamer]);
-  const play = () => simpleVideo.current?.play();
+  const play = () => {
+    simpleVideo.current?.play();
+    startWebcam(webcamStreamer, webcamVideo);
+  };
   const stop = () => {
     simpleVideo.current?.pause();
     simpleVideo.current?.load();
+    webcamStreamer.stop();
   };
   return (
-    <div className="App">
+    <Container className="App">
       <div>
         <button onClick={play}>Start</button>
         <button onClick={stop}>Stop</button>
       </div>
-      <video
+      <VideoStyle
         ref={simpleVideo}
         src="https://www.w3schools.com/html/mov_bbb.mp4"
         playsInline
       />
-      <video ref={webcamVideo} muted autoPlay playsInline />
-    </div>
+      <WebcamStyle ref={webcamVideo} muted autoPlay playsInline />
+    </Container>
   );
 }
 
